50971ff4f89b89b4287f44537e82067932271627
yoriWang <yoriwang@outlook.com>
2023-09-27 17:29:24 +0800

    类的定义-方法

---
+++ Java/study_array/src/com/learning/OOP/method/Employee.java
@@ -0,0 +1,16 @@
+package com.learning.OOP.method;
+
+/**
+ * ClassName: Employee
+ * Description:
+ *
+ * @author: yurenwang
+ * @create: 2023/9/22 17:15
+ * @version: 1.0
+ */
+public class Employee {
+    String name;
+    int age;
+    char gender;
+    MyDate birthDay;
+}
